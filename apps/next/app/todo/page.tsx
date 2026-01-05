4637c86bc1a7eebbd6f86c973afd56663cc0aa04
Jay Parmar <53110999+jayparmar11@users.noreply.github.com>
2026-01-05 11:05:01 +0000

    added todo web style

--- apps/next/app/todo/page.tsx
+++ apps/next/app/todo/page.tsx
@@ -2,4 +2,21 @@
 
 import TodoScreen from 'app/features/todo/todo-screen'
 
-export default TodoScreen
+function TodoPage() {
+  return (
+    <div
+      style={{
+        maxWidth: 600,
+        width: '100%',
+        display: 'flex',
+        height: '100%',
+        margin: 'auto',
+        minHeight: '100vh',
+      }}
+    >
+      <TodoScreen />
+    </div>
+  )
+}
+
+export default TodoPage
